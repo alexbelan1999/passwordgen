f719e92d9a14a3cbd2b0eff190088914b8eeeaa6
Aleksey Belan <aleksey.belan@lacit.net>
2020-11-20 11:52:28 +0300

    Add new file

---
+++ passgen.py
@@ -0,0 +1,15 @@
+import random
+
+symbols = '_()+-/*!&$#?=@<>'
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase = 'abcdefghijklnopqrstuvwxyz'
+numbers = '1234567890'
+
+number = int(input('Количество паролей: '))
+length = int(input('Длина пароля: '))
+
+for n in range(number):
+    password = ''
+    for i in range(length):
+        password += random.choice(symbols + uppercase + lowercase + numbers)
+    print(f'Пароль [{n}] {password}')
